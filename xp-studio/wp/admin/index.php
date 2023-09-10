f52b92b1b16e8b48a79adf899f49513455d3ff17
applh.com <contact@applh.com>
2023-09-10 12:41:55 +0200

    v0.3.3 (element-plus) table-tree, tree, calendar

--- xp-studio/wp/admin/index.php
+++ xp-studio/wp/admin/index.php
@@ -9,7 +9,19 @@ if (!is_callable('add_action')) {
 <template id="template-app">
     <h1>XP Studio</h1>
     <button>{{ message }}</button>
+    <hr/>
     <el-button type="success">Element Plus is ready</el-button>
+    <hr/>
+    <el-tree :data="tree_data" show-checkbox draggable></el-tree>
+    <hr/>
+    <el-tree :data="tree_data" show-checkbox draggable></el-tree>
+    <hr/>
+    <el-table :data="tree_data" row-key="id" :tree-props="{ children: 'children' }">
+        <el-table-column prop="id"></el-table-column>
+        <el-table-column prop="label"></el-table-column>
+    </el-table>
+    <hr/>
+    <el-calendar v-model="cal_date" />
 </template>
 
 <!-- import map -->
@@ -27,6 +39,23 @@ if (!is_callable('add_action')) {
 
     let data = {
         message: "hello Vue",
+        cal_date: new Date(),
+        tree_data: [
+        {
+            id: 1,
+            label: 'Level one 1',
+            children: [
+                {
+                    id: 4,
+                    label: 'Level two 1-1',
+                },
+            ]
+        },
+        {
+            id: 2,
+            label: 'Level one 2',
+        },
+    ],
     };
 
     // create the vue app
