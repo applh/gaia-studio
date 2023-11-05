185a0ffd8d6e8adb8e81feda7cad44f335d4ea4a
applh.com <contact@applh.com>
2023-11-05 22:11:44 +0100

    v0.6.0 (php) uikit template

--- php/templates/index.php
+++ php/templates/index.php
@@ -8,97 +8,118 @@ $now = date('Y-m-d H:i:s');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gaia Studio</title>
+    <title>UIKIT</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.17.8/dist/css/uikit.min.css" />
     <style>
-        html, body {
-            width: 100%;
-            height: 100%;
-            min-height: 100vh;
-            margin: 0;
-            padding: 0;
-            font-family: 'noto-emoji', Arial, Helvetica, sans-serif;
+        header, footer {
+            text-align: center;
+        }
+        /* .s2 even  bacground color gray */
+        .s2:nth-child(even) {
             background-color: #ccc;
-            min-height: 100vw;
-            font-size:2vw;
         }
-        * {
-            box-sizing: border-box;
-        }
-        main {
-            /* border: 1px solid #000; */
-        }
-        footer {
-            padding: 2rem;
-        }
-        h1 {
-            margin: 0;
-            padding: 2rem;
-            color: red;
-            text-align: center;
-            font-size: 2rem;
-        }
-        h2, h3 {
+        h1, h2, h3 {
             text-align: center;
         }
-        p {
+        .s2 img {
+            width: 100%;
+        }
+        main p {
+            max-width: 800px;
+            display: block;
+            margin: auto;
+            text-align: justify;
             padding: 1rem;
         }
-        img {
-            width: 100%;
-            height: auto;
-        }
-        .info {
-            text-align: center;
-        }
-
-        /* no margin or padding when printing */
-        @media print {
-            @page {
-                margin: 0;
-                padding: 0;
-            }
-            /* add page break between section */
-            section {
-                page-break-before: always;
-                page-break-after: always;
-            }
-        }
     </style>
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.17.8/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.17.8/dist/js/uikit-icons.min.js"></script>
+
 </head>
 
 <body>
-    <header></header>
-    <main>
-        <h1>Hello <?php echo $now; ?></h1>
-        <h1>test emoji 🔥 ✅ 🎁 ⭐️</h1>
-        <img src="/assets/photo.jpg" alt="photo">
-        <section class="s2">
-            <h2>Section 2</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, voluptatem nobis quod a non sit praesentium enim illo. Ex ad repellendus nobis commodi hic iure est cumque, tenetur repellat quas!</p>
-            <section class="h3">
-                <h3>title 3</h3>
-                <img src="/assets/photo.jpg" alt="" srcset="">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-            </section>
-            <section class="h3">
-                <h3>title 3</h3>
-                <img src="/assets/photo.jpg" alt="" srcset="">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-            </section>
-            <section class="h3">
-                <h3>title 3</h3>
-                <img src="/assets/photo.jpg" alt="" srcset="">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-            </section>
-            <section class="h3">
-                <h3>title 3</h3>
-                <img src="/assets/photo.jpg" alt="" srcset="">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-            </section>
+    <header class="uk-section">
+        <div class="uk-container">
+            <p><?php echo $now ?></p>
+        </div>
+    </header>
+    <main class="uk-container-expand">
+        <div class="uk-container">
+            <h1>UIKIT</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+        </div>
+        <section class="s2 uk-section">
+            <h2>title2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+            <div data-src="/assets/photo.jpg" sizes="" uk-img class="uk-height-medium"></div>
+            <div class="uk-grid uk-child-width-1-2@s uk-child-width-1-3@m">
+                <section class="uk-section">
+                    <h3>title3</h3>
+                    <img src="/assets/photo.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+                </section>
+                <section class="uk-section">
+                    <h3>title3</h3>
+                    <img src="/assets/photo.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+                </section>
+                <section class="uk-section">
+                    <h3>title3</h3>
+                    <img src="/assets/photo.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+                </section>
+            </div>
+        </section>
+        <section class="s2 uk-section">
+            <h2>title2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+            <img src="/assets/photo.jpg" alt="">
+            <div class="uk-grid uk-child-width-1-2@s uk-child-width-1-3@m">
+                <section class="uk-section">
+                    <h3>title3</h3>
+                    <img src="/assets/photo.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+                </section>
+                <section class="uk-section">
+                    <h3>title3</h3>
+                    <img src="/assets/photo.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+                </section>
+                <section class="uk-section">
+                    <h3>title3</h3>
+                    <img src="/assets/photo.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+                </section>
+            </div>
+        </section>
+        <section class="s2 uk-section">
+            <h2>title2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+            <img src="/assets/photo.jpg" alt="">
+            <div class="uk-grid uk-child-width-1-2@s uk-child-width-1-3@m">
+                <section class="uk-section">
+                    <h3>title3</h3>
+                    <img src="/assets/photo.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+                </section>
+                <section class="uk-section">
+                    <h3>title3</h3>
+                    <img src="/assets/photo.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+                </section>
+                <section class="uk-section">
+                    <h3>title3</h3>
+                    <img src="/assets/photo.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quo sed, unde voluptatem commodi rerum harum quidem atque. Et consequuntur tempore similique dolor ratione explicabo necessitatibus nihil asperiores dolores suscipit?</p>
+                </section>
         </section>
     </main>
-    <footer>
-        <div class="info"></div>
+    <footer class="uk-section">
+        <div class="uk-container">
+            <p>FOOTER</p>
+            <div class="info"></div>
+        </div>
     </footer>
     <script type="module">
         let ww = window.innerWidth;
@@ -114,6 +135,7 @@ $now = date('Y-m-d H:i:s');
         act_resize();
         window.addEventListener("resize", act_resize);
     </script>
+
 </body>
 
 </html>
